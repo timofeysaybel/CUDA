f21b9fe34369f836d66eeeeb1ab8f764712406b3
timofeysaybel <saybeltimofey@gmail.com>
2021-10-04 13:55:16 +0300

    "task1"

--- testSmall.sh
+++ testSmall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-for (( i = 1; i <= 5; i++))
-do
-	./ImageConvolution gaussian small/small$i
-	./ImageConvolution edge small/small$i
-	./ImageConvolution sharpen small/small$i
-done
-
+./ImageConvolution gaussian small/small1 small/small2 small/small3 small/small4 small/small5
+./ImageConvolution edge small/small1 small/small2 small/small3 small/small4 small/small5
+./ImageConvolution sharpen small/small1 small/small2 small/small3 small/small4 small/small5
